7320e60cae9a325e53170f025bee5b781421b037
Esebame1 <37189955+Esebame1@users.noreply.github.com>
2018-10-01 05:55:26 +0100

    Create Week_17_solutions.py

---
+++ submissions/Week_17_solutions.py
@@ -0,0 +1,24 @@
+Question 1 done
+
+a = input('Enter your name: ')
+b = int(input('Enter your age: '))
+print('Your name is', a)
+print('Your age is', b)
+
+a = input('Enter your name: ')
+b = int(input('Enter your age: '))
+c = 2018 + (100 - b)
+print('Welcome ' + str(a)+ ', You will be 100 years old in year ' + str(c))
+
+a = int(input('Enter A: '))
+b = int(input('Enter B: '))
+print('The addition of A & B gives', a+b)
+print('The substraction of B from A gives', a-b)
+print('The division of B from A gives', a/b)
+print('The multiplication of A & B gives', a*b)
+print('The modulo of A from B', a%b)
+
+Use len; that is if the list is:
+a = [0, 1, 2,3]
+print(len(a))
+The code above prints the length of list a
